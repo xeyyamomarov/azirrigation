c8c5ae6d225b3a52b3e540604ba2c420dd4fc9cd
unknown <xeyyamomarov2014@gmail.com>
2023-04-17 15:18:22 +0400

    /

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ const menuIcon = document.getElementById("menu-icon");
 const navbar = document.getElementsByClassName("navbar")[0];
 const nav = document.querySelectorAll("nav a");
 const header = document.getElementsByClassName("header")[0];
+// const form = document.querySelectorAll("form")[0];
+
+// for (let i = 0; i < form.length; i++) {
+//   form[i].addEventListener("change", (e) => console.log(e.target.value));
+// }
 
 for (let i = 0; i < nav.length; i++) {
   nav[i].addEventListener("click", () => {
@@ -11,14 +16,16 @@ for (let i = 0; i < nav.length; i++) {
 
 menuIcon.addEventListener("click", () => {
   navbar.classList.toggle("active");
-  header.classList.toggle("head")
+  header.classList.toggle("head");
 });
 
 document.addEventListener("scroll", () => {
   header.classList.toggle("sticky", window.scrollY > 80);
 });
-// document.addEventListener("click",()=>{
-//   header.classList.toggle("active")
-// })
 
-// sticky navbar
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+};
+
+// form.addEventListener("submit", handleSubmit);
---
+++ script.js
@@ -0,0 +1,20 @@
+const menuIcon = document.getElementById("menu-icon");
+const navbar = document.getElementsByClassName("navbar")[0];
+const nav = document.querySelectorAll("nav a");
+const header = document.getElementsByClassName("header")[0];
+
+for (let i = 0; i < nav.length; i++) {
+  nav[i].addEventListener("click", () => {
+    navbar.classList.remove("active");
+  });
+}
+
+menuIcon.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  header.classList.toggle("sticky", window.scrollY > 80);
+});
+
+// sticky navbar
